78e462fbe9db47c9935d2ab77465563797392280
penugantiruthvijkumar <penugantiruthvijkumar@gmail.com>
2026-02-17 09:30:12 +0530

    branch

--- type.py
+++ type.py
@@ -1,16 +1,8 @@
-x="ruthvij"
-print(type(x))
-
-z=("can","me","hello")
-print(type(z))
-
-a=3
-b=2
-if a>b:
-    print("hello")
-else:
-    print("go back")
-
-for i in range(0,3):
-    a=a<<i
-print(a)    
+def fun(n):
+    if n%2==0: 
+        return "even"
+    else :
+        return "odd"
+    
+x=input("enter")
+print(fun(int(x)))
